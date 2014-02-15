35e6ead83a1cc2c2cb5309da1f1f0bfca71354c8
Nico Weinert <nico.weinert@googlemail.com>
2014-02-15 18:01:04 +0000

    anti social data iwth pandas

--- urbandata/parser.py
+++ urbandata/parser.py
@@ -1,6 +1,4 @@
 import os
-import datetime as dt
-
 import pandas as pd
 
 DATA_DIR = os.path.join(os.path.dirname(os.getcwd()), 'data')
@@ -8,38 +6,22 @@ ANTI_SOCIAL_FN = os.path.join(DATA_DIR, 'WCC_CleansingAntiSocialBehaviour.csv')
 LICENSING_FN = os.path.join(DATA_DIR, 'WCC_Licensing.csv')
 
 
-def read_file(fn):
-    def parse_row(row):
-        return row.strip().replace('"', '').split(",")
-
-    with open(fn, 'r') as _file:
-        header = parse_row(_file.next())
-        header = [h.title() for h in header]
-        data = map(parse_row, _file)
-        data = zip(*data)
-        return dict(zip(header, data))
-
-
 def get_anti_social_data():
-    raw_data = read_file(ANTI_SOCIAL_FN)
-    result = {}
-    result['date'] = [dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-                      for x in raw_data['Eventdate']]
-    for col in ['Blood', 'Urine', 'Vomit', 'Humanfouling']:
-        result[col] = [x == 'YES' for x in raw_data[col]]
-    for col in ['Lat', 'Long']:
-        result[col] = map(float, raw_data[col])
-    return result
+     df = pd.read_csv(ANTI_SOCIAL_FN,
+                      index_col=["EventDate"], true_values=['Yes'], false_values=['NO'])
+     df.columns = [c.title() for c in df.columns]
+     return df
 
 
 def get_licensing_data():
-    return pd.read_csv(LICENSING_FN,
-                       index_col=["RepresentationResponseDate"],
-                       parse_dates=["RepresentationResponseDate"])
+     df = pd.read_csv(LICENSING_FN,
+                      index_col=["RepresentationResponseDate"],
+                      parse_dates=["RepresentationResponseDate"])
+     df.columns = [c.title() for c in df.columns]
+     return df
 
 
 if __name__ == '__main__':
-    raw_data = read_file(ANTI_SOCIAL_FN)
-    data = get_anti_social_data()
-    print data['Blood']
-    print data['date']
+    print get_anti_social_data()
+    print get_licensing_data()
+
