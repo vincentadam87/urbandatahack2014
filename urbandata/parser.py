d106e2a207ea9dc548c6c2e0d163e21e37d0f356
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-02-15 18:01:17 +0000

    Parse licensing data with Pandas

--- urbandata/parser.py
+++ urbandata/parser.py
@@ -7,21 +7,24 @@ LICENSING_FN = os.path.join(DATA_DIR, 'WCC_Licensing.csv')
 
 
 def get_anti_social_data():
-     df = pd.read_csv(ANTI_SOCIAL_FN,
-                      index_col=["EventDate"], true_values=['Yes'], false_values=['NO'])
-     df.columns = [c.title() for c in df.columns]
-     return df
+    df = pd.read_csv(ANTI_SOCIAL_FN,
+                     index_col=["EventDate"],
+                     parse_dates=["EventDate"],
+                     true_values=["Yes"],
+                     false_values=["NO"],
+                     converters={"StreetName": lambda s: s.title()})
+    df.columns = [c.title() for c in df.columns]
+    return df
 
 
 def get_licensing_data():
-     df = pd.read_csv(LICENSING_FN,
-                      index_col=["RepresentationResponseDate"],
-                      parse_dates=["RepresentationResponseDate"])
-     df.columns = [c.title() for c in df.columns]
-     return df
+    df = pd.read_csv(LICENSING_FN,
+                     index_col=["RepresentationResponseDate"],
+                     parse_dates=["RepresentationResponseDate"])
+    df.columns = [c.title() for c in df.columns]
+    return df
 
 
 if __name__ == '__main__':
     print get_anti_social_data()
     print get_licensing_data()
-
